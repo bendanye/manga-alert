d98f26a7c92914ea6ecc42f096c8cd1770c6e2df
benjaminng <shen_soonng@hotmail.com>
2021-05-09 10:16:18 +0800

    find another manga website

--- src/managerscraper.js
+++ src/managerscraper.js
@@ -9,28 +9,11 @@ const getMangaListOfChapters = async(page, mangaTitle) => {
 }
 
 const getMangaChaptersElement = async(page) => {
-    const isDescriptionLong = await isMangaDescriptionLong(page);
-
-    if (isDescriptionLong) {
-        await page.waitForXPath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/div[5]/ul[1]");
-        const [getXpath] = await page.$x("/html[1]/body[1]/div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/div[5]/ul[1]");
-        return getXpath;
-    } else {
-        await page.waitForXPath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/div[4]/ul[1]");
-        const [getXpath] = await page.$x("/html[1]/body[1]/div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/div[4]/ul[1]");
-        return getXpath;
-    }
-}
-
-const isMangaDescriptionLong = async(page) => {
-    try {
-        await page.waitForXPath("//body/div[@id='all']/div[@id='pusat']/div[@id='main']/div[@id='content']/div[1]/div[2]/div[3]/span[1]/img[1]", {
-            timeout: 5000
-        });
-        return true;
-    } catch {
-        return false;
-    }
+    const selector = "//tbody/tr[1]/td[1]/h4[1]/a[1]";
+    await page.waitForXPath(selector);
+    const [getXpath] = await page.$x(selector);
+    return getXpath;
+    
 }
 
 module.exports = {
